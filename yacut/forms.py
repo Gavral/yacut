8b857aed85d812196938994db5c6f5aa1ca885df
Gavral <gavrilov-al@mail.ru>
2023-01-12 20:27:23 +0300

    edits after the first review by the reviewer

--- yacut/forms.py
+++ yacut/forms.py
@@ -4,21 +4,28 @@ from wtforms.validators import URL, DataRequired, Length, Optional, Regexp
 
 from .constants import LINK_REG
 
+STRING_FIELD_NAME_ORIGINAL_LINK = 'Длинная ссылка'
+STRING_ORIGINAL_LINK_DATAREQUIRED = 'Обязательное поле'
+STRING_ORIGINAL_LINK_URL = 'Некорректная ссылка'
+STRING_FIELD_NAME_CUSTOM_ID = 'Ваш вариант короткой ссылки'
+STRING_CUSTOM_ID_REGEXP = 'Допустимы только цифры и буквы "a-Z"'
+STRING_FIELD_NAME_SUBMIT = 'Создать'
+
 
 class URLMapForm(FlaskForm):
     original_link = URLField(
-        'Длинная ссылка',
+        STRING_FIELD_NAME_ORIGINAL_LINK,
         validators=[
-            DataRequired(message='Обязательное поле'),
-            URL(message='Некорректная ссылка')]
+            DataRequired(message=STRING_ORIGINAL_LINK_DATAREQUIRED),
+            URL(message=STRING_ORIGINAL_LINK_URL)]
     )
     custom_id = StringField(
-        'Ваш вариант короткой ссылки',
+        STRING_FIELD_NAME_CUSTOM_ID,
         validators=[
             Optional(),
             Length(1, 16),
             Regexp(
                 regex=LINK_REG,
-                message='Допустимы только цифры и буквы "a-Z"')]
+                message=STRING_CUSTOM_ID_REGEXP)]
     )
-    submit = SubmitField('Создать')
+    submit = SubmitField(STRING_FIELD_NAME_SUBMIT)
